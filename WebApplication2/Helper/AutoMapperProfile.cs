133959febd4b47f34c4c2df432827b85a1a33eae
Thanh Lam <lamdoan1122334455@gmail.com>
2024-04-22 22:58:08 +0700

    Update

---
+++ WebApplication2/Helper/AutoMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using WebApplication2.Data;
+using WebApplication2.ViewModels;
+
+namespace WebApplication2.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<RegisterVM, KhachHang>();
+            //.ForMember(kh => kh.HoTen, option => option.MapFrom(RegisterVM => RegisterVM.HoTen))
+            //.ReverseMap();
+        }
+    }
+}
---
+++ WebApplication2/ViewModels/LoginVM.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.ViewModels
+{
+    public class LoginVM
+    {
+        [Display(Name = "Mã công dân")]
+        [Required(ErrorMessage = "Vui lòng điền trường này")]
+        public string MaKh { get; set; }
+
+        [Display(Name = "Mật khẩu")]
+        [Required(ErrorMessage = "Vui lòng điền trường này")]
+        [DataType(DataType.Password)]
+        public string MatKhau { get; set; }
+    }
+}
